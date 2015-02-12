9c01c6f578175012e3c7c42a581f5d91872583ee
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-02-12 00:57:58 -0500

    commit

--- calculator.js
+++ calculator.js
@@ -1,9 +1,29 @@
-var calculate = 100;
+var calculate = 0;
 
 $(".display .numbers").text(calculate);
 
-// var operation = function(input, action, inputTwo){
-//   answer = (input+action+inputTwo);
-//   return answer;
-// };
+var operation = function(input, action, inputTwo) {
+  answer = input+action+inputTwo;
+  return answer;
+};
 
+$(".display .numbers").on("click", "a", function(event){
+      calculate = operation;
+      });
+
+$(".clear").on("click", "a", function(event){
+      calculate = operation;
+      });
+
+$(".actions").on("click", "a", function(event){
+      calculate = operation;
+      });
+
+$(".numbers .equals").on("click", "a", function(event){
+      calculate = operation;
+      });
+
+// if number is clicked, number should appear in display
+// if action is clicked, should apply to that number
+// clicking equals displays the answer
+// clicking c clears the display
