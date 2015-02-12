246734dbd134ae45619339cc8b06715faddee8df
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-02-12 00:02:53 -0500

    initial commit

---
+++ calculator.js
@@ -0,0 +1,9 @@
+var calculate = 100;
+
+$(".display .numbers").text(calculate);
+
+// var operation = function(input, action, inputTwo){
+//   answer = (input+action+inputTwo);
+//   return answer;
+// };
+
