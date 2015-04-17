1a4793efda5d953da5c90dde34ff37910ab26b13
Matt Wilkerson <mattisonwilkerson@gmail.com>
2015-04-16 23:06:57 -0400

    added math functionality working

--- calculator.js
+++ calculator.js
@@ -1,29 +1,80 @@
-var calculate = 0;
+var initiate = 0;
+var one = "";
+var two = "";
+var operator = null;
+var operators = ["+", "-", "*", "/"];
 
-$(".display .numbers").text(calculate);
+var display = function(numInput) {
+  numInput = numInput.toString()
+  if (numInput.length > 10) {
+    numInput = "Error";
+  }
+  $(".displayed-numbers").text(numInput);
+}
 
-var operation = function(input, action, inputTwo) {
-  answer = input+action+inputTwo;
-  return answer;
-};
+var background = function() {
+  return operator !== null;
+}
 
-$(".display .numbers").on("click", "a", function(event){
-      calculate = operation;
-      });
+$(function(){
+  $(".operator-btn").click(function(e){
+    var target = e.currentTarget;
+    var elem = $(target);
+    var opt = elem.text();
+    if (opt && one && two) {
+      var sum = eval(one + operator + two);
+      display(sum);
+      two = "";
+      one = sum;
+    }
+    operator = opt
+  })
 
-$(".clear").on("click", "a", function(event){
-      calculate = operation;
-      });
+  $(".number").click(function(e){
+    var target = e.currentTarget;
+    var elem = $(target);
+    var text = elem.text();
+    console.log("number" + text); 
+    if (text == "=") {
+      var sum = eval(one + operator + two);
+      display(sum);
+      one = sum;
+      two = "";
+      operator = null;
+      return;
+    }
+    if (background()) {
+      two += text;
+      display(two);
+    } else {
+      one += text;
+      display(one);
+    }
+  })
 
-$(".actions").on("click", "a", function(event){
-      calculate = operation;
-      });
+  $(".clear").click(function(e) {
+    var clearTarget = e.currentTarget;
+    one = "";
+    two = "";
+    operator = null;
+    display(0);
+  })
 
-$(".numbers .equals").on("click", "a", function(event){
-      calculate = operation;
-      });
+  // Make the operators red when clicked
+  $(".divide").on("click", function(){
+    $(".divide-action").css({"color": "red"});
+    });
+  $(".multiply").on("click", function(){
+    $(".multiply-action").css({"color": "red"});
+    });
+  $(".subtract").on("click", function(){
+    $(".subtract-action").css({"color": "red"});
+    });
+  $(".add").on("click", function(){
+    $(".add-action").css({"color": "red"});
+    });
+  $(".equals").on("click", function(){
+    $(".action").css({"color": "#999cb0"});
+    });
 
-// if number is clicked, number should appear in display
-// if action is clicked, should apply to that number
-// clicking equals displays the answer
-// clicking c clears the display
+});
